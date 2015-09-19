3954e51a3ea131e5f622afc3eacbc4ea2ca54b07
Daniel Au <dakmau@gmail.com>
2015-09-19 14:34:41 -0400

    Remove authenticity token verification

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      skip_before_filter  :verify_authenticity_token
+
       def create
         user = User.create(name: params[:name])
         if user.save
