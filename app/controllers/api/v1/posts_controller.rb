ff610a0c28da67f15b1f28092c54691ea3c4ae18
Daniel Au <dakmau@gmail.com>
2015-09-19 18:03:21 -0400

    Fix indico sentiments

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -8,7 +8,7 @@ module Api
       def create
         post = Post.create(message: params[:message], latitude: params[:latitude], longitude: params[:longitude])
         Indico.api_key = '6b911bcc3dc867c5d5b9946b2f5b3345'
-        post.positivity = Indico.sentiment(message)
+        post.positivity = Indico.sentiment(post.message)
         if post.save
           render json: {status: 200}
         end
