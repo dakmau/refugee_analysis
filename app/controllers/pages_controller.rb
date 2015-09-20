c7e779026a82a2108f0c552230a82130c63d5ade
Daniel Au <dakmau@gmail.com>
2015-09-20 04:48:57 -0400

    Add trending

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,10 @@ class PagesController < ApplicationController
     @posts = Post.all.limit(100)
     @users = User.all.limit(100)
 
-    #keywords
-    @berlin_tags = Tag.near("Berlin", 150).limit(5)
-    @istanbul_tags = Tag.near("Istanbul", 150).limit(5)
-    @toronto_tags = Tag.near("Toronto", 150).limit(5)
+    # keywords
+    @berlin_tags = most_common(Tag.near("Berlin", 150).map {|n| n.word})
+    @istanbul_tags = most_common(Tag.near("Istanbul", 150).map {|n| n.word})
+    @toronto_tags = most_common(Tag.near("Toronto", 150).map {|n| n.word})
   end
 
   def refugee_search
@@ -20,5 +20,15 @@ class PagesController < ApplicationController
       redirect_to(:back)
       return
     end
+  end
+
+  private
+
+  def most_common posts
+    url_count = posts.each_with_object(Hash.new(0)) do |item, count|
+      count[item] += 1
+    end
+    top_urls = url_count.keys.sort_by!{|url| url_count[url]}.last(5).reverse!
+    return top_urls
   end
 end
